6579cc0a459597e76feea28b86c36f0958d1a943
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-12-28 11:34:48 +0100

    fix(ch05): fix test name

--- src/ch-05_pure-functions/5.1_pure-functions.test.js
+++ src/ch-05_pure-functions/5.1_pure-functions.test.js
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import {double} from "./5.1_pure-functions";
 
 describe("Pure Functions", () => {
-    it("should always return the same output", () => {
+    it("should always return the same output for the same input", () => {
         expect(double(2)).to.equal(4);
         expect(double(2)).to.equal(4);
         expect(double(3)).to.equal(6);
