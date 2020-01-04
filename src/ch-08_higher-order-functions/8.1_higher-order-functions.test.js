714f8ecc038ce22da1ec7bebc39c867869b101ad
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-01-04 14:01:36 +0100

    feat(ch08): add more high order function examples
    use custom filter and built-in array filter

--- src/ch-08_higher-order-functions/8.1_higher-order-functions.test.js
+++ src/ch-08_higher-order-functions/8.1_higher-order-functions.test.js
@@ -12,5 +12,18 @@ describe("High Order Functions", () => {
         const censor = words => filter(word => word.length !== 4, words);
         const words = ["hallo", "chao", "servus"];
         expect(censor(words)).to.deep.equal(["hallo", "servus"]);
+    });
+
+    it("should show how a filter works in a starts with S function", () => {
+        const startsWithS = words => filter(word => word.startsWith('s'), words);
+        const words = ["hallo", "chao", "servus"];
+        expect(startsWithS(words)).to.deep.equal(["servus"]);
+    });
+
+    it("should show how the built-in filter works with a highpass function", () => {
+        const highpass = cutoff => x => x >= cutoff;
+        const gte4 = highpass(4);
+        const numbers = [1, 3, 5, 6];
+        expect(numbers.filter(gte4)).to.deep.equal([5, 6]);
     })
 });
