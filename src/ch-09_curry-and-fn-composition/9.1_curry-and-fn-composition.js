c0b5fc2c500fb3865b63c4540d0eebf8a367b171
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-01-05 18:32:22 +0100

    feat(ch09): add function composition of n functions example

--- src/ch-09_curry-and-fn-composition/9.1_curry-and-fn-composition.js
+++ src/ch-09_curry-and-fn-composition/9.1_curry-and-fn-composition.js
@@ -1,1 +1,3 @@
 export const sum = a => b => a + b;
+
+export const compose = (...fns) => x => fns.reduceRight((acum, f) => f(acum), x);
--- src/ch-09_curry-and-fn-composition/9.1_curry-and-fn-composition.test.js
+++ src/ch-09_curry-and-fn-composition/9.1_curry-and-fn-composition.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {sum} from "./9.1_curry-and-fn-composition";
+import {compose, sum} from "./9.1_curry-and-fn-composition";
 
 describe("Curry and Function Composition", () => {
     it("should apply two functions in a curried form", () => {
@@ -13,13 +13,21 @@ describe("Curry and Function Composition", () => {
     });
 
     it("should allow function composition of two functions", () => {
-        const g = a => a + 1;
-        const f = b => b * 2;
+        const f = a => a * 2;
+        const g = b => b + 1;
         const h = x => f(g(x));
 
         const composeFn = (f, g) => x => f(g(x));
 
         expect(h(30)).to.equal(62);
         expect(composeFn(f,g)(30)).to.equal(62);
+    });
+
+    it("should allow function composition of n functions", () => {
+        const f = a => a * 2;
+        const g = b => b + 1;
+        const h = c => c * 3;
+
+        expect(compose(f, g, h)(20)).to.equal(122);
     });
 });
