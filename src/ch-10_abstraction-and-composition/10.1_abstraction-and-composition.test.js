cd253e9a2526213d5c81a3fb601422653a3a3500
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-01-18 12:31:44 +0100

    feat(ch10): curry the add function

--- src/ch-10_abstraction-and-composition/10.1_abstraction-and-composition.test.js
+++ src/ch-10_abstraction-and-composition/10.1_abstraction-and-composition.test.js
@@ -1,11 +1,19 @@
-import {assert, expect} from 'chai';
-import {add} from "./10.1_abstraction_and_composition";
+import {expect} from 'chai';
+import {add, addN} from "./10.1_abstraction_and_composition";
 
 describe("Abstraction and Composition", () => {
     it("should fix a parameter in a function", () => {
-
         const a = add(1, 1);
         const b = add(a, 1);
+
+        expect(b).to.equal(3);
+    });
+
+    it("should curry the add function", () => {
+        const inc = addN(1);
+
+        const a = inc(1);
+        const b = inc(a);
 
         expect(b).to.equal(3);
     });
--- src/ch-10_abstraction-and-composition/10.1_abstraction_and_composition.js
+++ src/ch-10_abstraction-and-composition/10.1_abstraction_and_composition.js
@@ -1,1 +1,3 @@
 export const add = (a, b) => a + b;
+
+export const addN = a => b => a + b;
