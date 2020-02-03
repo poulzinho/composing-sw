73b2fb23e42f9882f9c992c53addadb1101302d4
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-02-03 08:29:52 +0100

    feat(ch11): ensure function composition law

--- src/ch-11_functors-and-categories/11.1_functors_and_categories.test.js
+++ src/ch-11_functors-and-categories/11.1_functors_and_categories.test.js
@@ -7,4 +7,16 @@ describe("Functors and Categories", () => {
 
         expect(a).to.deep.equal(b);
     });
+
+    it("should ensure function composition", () => {
+        const add1 = n => n + 1;
+        const doubleIt = n => n * 2;
+
+        const mappable = [30];
+
+        const res1 = mappable.map(add1).map(doubleIt);
+        const res2 = mappable.map(x => doubleIt(add1(x)));
+
+        expect(res1).to.deep.equal(res2);
+    })
 });
