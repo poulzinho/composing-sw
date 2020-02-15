39b0219db161c8d7baf4896159dbf0a1b967d593
Paul Gualotuna <p.gualotuna.dev@gmail.com>
2020-02-15 20:16:39 +0100

    feat(ch12): add Monad example

--- src/ch-12_monads/12.1_monads.test.js
+++ src/ch-12_monads/12.1_monads.test.js
@@ -1,4 +1,4 @@
-import {assert, expect} from 'chai';
+import {expect} from 'chai';
 import {flatMap} from "./12.1_monads";
 
 describe("Monads", () => {
@@ -6,5 +6,37 @@ describe("Monads", () => {
         const echo = n => x => Array.from({length: n}).fill(x);
 
         expect(flatMap(echo(2), [1, 2, 3])).deep.equal([1, 1, 2, 2, 3, 3]);
-    })
+    });
+
+    it("should implement a Monad", () => {
+        const Monad = value => ({
+            flatMap: f => {
+                console.log('** flatMap');
+                console.log('-- f', f.toString());
+                console.log('-- value', value);
+                return f(value)
+            },
+            map(f) {
+                console.log('* map');
+                console.log('- f', f.toString());
+                console.log('- value', value);
+
+                return this.flatMap(a => {
+                    console.log('exec flatMap f');
+                    console.log('--- f', f.toString());
+                    console.log('--- a', a);
+                    console.log('--- f(a)', f(a));
+                    console.log('--- Monad.of', Monad.of(f(a)));
+                    return Monad.of(f(a))
+                });
+            }
+        });
+
+        Monad.of = x => {
+            console.log('*** Monad(x)', x);
+            return Monad(x)
+        };
+
+        expect(Monad(21).map(x => x * 2).flatMap(x => x)).equal(42);
+    });
 });
