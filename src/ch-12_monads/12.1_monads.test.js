2e1b10c7a98679e643def3d91c387f9158bcfc08
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-02-16 19:15:09 +0100

    feat(ch12): add Kleisli composition

--- src/ch-12_monads/12.1_monads.test.js
+++ src/ch-12_monads/12.1_monads.test.js
@@ -8,6 +8,27 @@ describe("Monads", () => {
         expect(flatMap(echo(2), [1, 2, 3])).deep.equal([1, 1, 2, 2, 3, 3]);
     });
 
+    it("should allow composition and type lifting, Kleisli composition", () => {
+
+        const composeM = method => (...ms) => (
+            ms.reduce((f, g) => x => g(x)[method](f))
+        );
+
+        const composePromises = composeM('then');
+
+        const getUserById = id => id === 2
+            ? Promise.resolve({name: 'Polo', role: 'Admin'})
+            : Promise.resolve({name: 'unknown', role: 'unknown'});
+
+        const hasPermission = ({role}) => (Promise.resolve(role === 'Admin'));
+
+        const authUser = composePromises(hasPermission, getUserById);
+
+        return authUser(3).then((authorization) => {
+            expect(authorization).equal(false);
+        });
+    });
+
     it("should implement a Monad", () => {
         const double = x => x * 2;
         expect(Monad(21).map(double).flatMap(x => x)).equal(42);
