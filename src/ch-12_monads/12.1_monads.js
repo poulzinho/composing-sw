d7f7381333e2b41192baad62d7454d01ec658c5a
Paul Gualotuna <p.gualotuna.dev@gmail.com>
2020-02-16 09:40:53 +0100

    feat(ch12): add Kleisli composition function

--- src/ch-12_monads/12.1_monads.js
+++ src/ch-12_monads/12.1_monads.js
@@ -2,28 +2,34 @@ export const flatMap = (f, arr) => [].concat(...arr.map(f));
 
 export const Monad = value => ({
     flatMap: f => {
+        /*
         console.log('** flatMap');
         console.log('-- f', f.toString());
         console.log('-- value', value);
+         */
         return f(value)
     },
     map(f) {
+        /*
         console.log('* map');
         console.log('- f', f.toString());
         console.log('- value', value);
+         */
 
         return this.flatMap(a => {
+            /*
             console.log('exec flatMap f');
             console.log('--- f', f.toString());
             console.log('--- a', a);
             console.log('--- f(a)', f(a));
             console.log('--- Monad.of', Monad.of(f(a)));
+             */
             return Monad.of(f(a))
         });
     }
 });
 
 Monad.of = x => {
-    console.log('*** Monad(x)', x);
+    // console.log('*** Monad(x)', x);
     return Monad(x)
 };
