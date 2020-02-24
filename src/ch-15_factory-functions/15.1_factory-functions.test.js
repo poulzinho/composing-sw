e36b0f7b1cd1f13db6abb0d00e994e8649188c58
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-02-24 07:50:30 +0100

    feat(ch15): support factory functions for mixin composition

--- src/ch-15_factory-functions/15.1_factory-functions.test.js
+++ src/ch-15_factory-functions/15.1_factory-functions.test.js
@@ -1,4 +1,6 @@
 import {expect} from 'chai';
+import {withConstructor} from "./15.1_factory_functions";
+import {pipe} from "../ch-10_abstraction-and-composition/10.1_abstraction_and_composition";
 
 describe("Factory Functions", () => {
 
@@ -45,10 +47,12 @@ describe("Factory Functions", () => {
     });
 
     it("should support factory functions with default parameters", () => {
-        const createUser = ({
-            userName = "anon",
-            avatar = "anon.png"
-        } = {}) => ({
+        const createUser = (
+            {
+                userName = "anon",
+                avatar = "anon.png"
+            } = {}
+        ) => ({
             userName,
             avatar,
         });
@@ -57,6 +61,57 @@ describe("Factory Functions", () => {
 
         expect(aUser.userName).equal("anon");
         expect(aUser.avatar).equal("anon.png");
+    });
+
+    it("should support factory functions for mixin composition", () => {
+
+        const withFlying = obj => {
+            let isFlying = false;
+            return {
+                ...obj,
+                fly() {
+                    isFlying = true;
+                    return this;
+                },
+                land() {
+                    isFlying = false;
+                    return this;
+                },
+                isFlying: () => isFlying
+            }
+        };
+
+        const withBattery = ({capacity}) => obj => {
+            let percentCharged = 100;
+            return {
+                ...obj,
+                draw(percent) {
+                    const remaining = percentCharged - percent;
+                    percentCharged = remaining > 0 ? remaining : 0;
+                    return this;
+                },
+                getCharge: () => percentCharged,
+                getCapacity: () => capacity,
+            }
+        };
+
+        const createDrone = ({capacity = '3000mAh'}) => pipe(
+            withFlying,
+            withBattery({capacity}),
+            withConstructor(createDrone)
+        )({});
+
+        const myDrone = createDrone({capacity: '5500mAh'});
+
+        console.log('my drone', myDrone.toString());
+
+        expect(myDrone.fly().isFlying()).to.be.true;
+        expect(myDrone.land().isFlying()).to.be.false;
+        expect(myDrone.getCapacity()).to.equal("5500mAh");
+        expect(myDrone.draw(50).getCharge()).equal(50);
+        expect(myDrone.draw(75).getCharge()).equal(0);
+        expect(myDrone.constructor).to.deep.equal(createDrone);
+
     });
 
 });
