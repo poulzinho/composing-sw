b81412efa47c4b31a677aed362f25a563488934f
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2020-03-08 17:24:09 +0100

    feat(ch19): add pure functions to view and set with lenses

--- src/ch-19_lenses/ch-19_lenses.test.js
+++ src/ch-19_lenses/ch-19_lenses.test.js
@@ -17,4 +17,36 @@ describe("Lenses", () => {
         expect(setY([10, 20, 30])(2000)).deep.equal([10, 2000, 30]);
     });
 
+    it("should use pure functions to view and set with lenses", () => {
+        // pure functions
+        const view = (lens, store) => lens.view(store);
+        const set = (lens, value, store) => lens.set(value, store);
+
+        // lens function returns accessors for a prop
+        const lens = prop => ({
+            view: store => store[prop],
+            set: (value, store) => ({
+                ...store,
+                [prop]: value
+            })
+        });
+
+        // object to be accessed with a lens
+        const initialStore = {
+            a: "polo",
+            b: "zaky",
+        };
+
+        const aLens = lens('a');
+        const bLens = lens('b');
+
+        // use view function
+        expect(view(aLens, initialStore)).equal('polo');
+        expect(view(bLens, initialStore)).equal('zaky');
+
+        // use set function
+        const finalStore = set(aLens, 'paulo', initialStore);
+        expect(view(aLens, finalStore)).equal('paulo');
+    });
+
 });
